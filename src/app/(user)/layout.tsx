9c3c30eaced00d000419274d646489916f149ac8
Jahidul Islam <jahidul.app@gmail.com>
2025-09-18 15:30:58 +0600

    chore: update port

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function UserLayout({ children }: React.PropsWithChildren) {
-  return <div>{children}</div>;
+  return (
+    <div className="mx-auto container max-w-md border-x min-h-svh relative px-6">
+      <main className="flex flex-col gap-6">{children}</main>
+    </div>
+  );
 }
